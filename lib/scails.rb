eaaf4d7a271a9c73c3b08528306f9d0b17590e68
simon-hicks <ruby@simonhicks.org>
2010-07-29 15:11:52 +0700

    moved shortcut methods into seperate module

--- lib/scails.rb
+++ lib/scails.rb
@@ -7,15 +7,6 @@ end
   require f
 end
 
-require File.expand_path(File.dirname(__FILE__) + '/scails/live')
-require File.expand_path(File.dirname(__FILE__) + '/scails/extensions')
-require File.expand_path(File.dirname(__FILE__) + '/scails/clock')
-require File.expand_path(File.dirname(__FILE__) + '/scails/metronome')
-require File.expand_path(File.dirname(__FILE__) + '/scails/instrument')
-require File.expand_path(File.dirname(__FILE__) + '/scails/midiator')
-require File.expand_path(File.dirname(__FILE__) + '/scails/constants')
-require File.expand_path(File.dirname(__FILE__) + '/scails/key')
-require File.expand_path(File.dirname(__FILE__) + '/scails/motive')
-require File.expand_path(File.dirname(__FILE__) + '/scails/pitch_class')
-require File.expand_path(File.dirname(__FILE__) + '/scails/chord')
-require File.expand_path(File.dirname(__FILE__) + '/scails/rhythm')
+%w(live extensions clock metronome instrument midiator constants key motive pitch_class chord rhythm shortcuts).each do |f|
+  require File.expand_path(File.dirname(__FILE__) + '/scails/' + f)
+end
